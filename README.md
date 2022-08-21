bbf1bd7cc8ba0f78c812507a23e1dcee50302ec1
thr3a <thr3a@example.com>
2022-08-22 08:06:09 +0900

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,10 @@
+# 利用方法
+
+```
+npx create-next-app hogehoge --example https://github.com/thr3a/nextjs-template
+```
+
+# TODO
+
+- データベース？
+- デプロイ簡単にする
---
+++ pages/page2.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import { Container } from '@mantine/core';
+import { IconDownload } from '@tabler/icons';
+
+const Page2: NextPage = () => {
+  return (
+    <Container>
+      <h1>page2</h1>
+      <IconDownload></IconDownload>
+    </Container>
+  );
+};
+
+export default Page2;
