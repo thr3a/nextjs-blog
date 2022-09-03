eaf7a18b72dce0142e103da46ead8fe2f2d26440
thr3a <thr3a@example.com>
2022-09-03 13:24:00 +0900

    fix:

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { NextPage, NextPageContext } from 'next';
-interface Props {
-    statusCode: number;
-}
-const Error: NextPage<Props> = ({ statusCode }) => {
-  return <div>{statusCode}エラーが発生しました</div>;
-};
+// import React from 'react';
+// import { NextPage, NextPageContext } from 'next';
+// interface Props {
+//     statusCode: number;
+// }
+// const Error: NextPage<Props> = ({ statusCode }) => {
+//   return <div>{statusCode}エラーが発生しました</div>;
+// };
 
-Error.getInitialProps = ({ res, err }: NextPageContext) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode ?? 500 : 404;
-  process.stderr.write(`${err}\n`);
-  return { statusCode };
-};
+// Error.getInitialProps = ({ res, err }: NextPageContext) => {
+//   const statusCode = res ? res.statusCode : err ? err.statusCode ?? 500 : 404;
+//   process.stderr.write(`${err}\n`);
+//   return { statusCode };
+// };
 
-export default Error;
+// export default Error;
